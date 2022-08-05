819657d431d02d7be424a6b5a5a61f9a9f02c471
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-08-05 13:39:46 +0000

    agora o slide veio

---
+++ Ondagil/js/slick.js
@@ -0,0 +1,8 @@
+$('.cards-mobile').slick({
+    arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          infinite: false,
+        
+  });
