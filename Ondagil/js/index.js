87cf5ae28d43d48246ed294a955435e8b2c332bf
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-08-06 17:14:22 -0300

    drawer mobile

--- Ondagil/js/index.js
+++ Ondagil/js/index.js
@@ -1,11 +1,14 @@
 const btnMobile = document.getElementById('btn-mobile');
+const drawer = document.getElementById('drawer');
+const fechaDrawer = document.getElementById('fecha-drawer');
 
-function toggleMenu(){
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+function toggleDrawer(){
+    drawer.classList.toggle('active');
+    fechaDrawer.classList.toggle('active');
 }
 
-btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('click', toggleDrawer);
+fechaDrawer.addEventListener('click', toggleDrawer);
 
 // PRINT DA LARGURA DA TELA
 console.log(window.innerWidth);
